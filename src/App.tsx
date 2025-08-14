93a694e5e078d85f5f72fcb4139b99e13b6130e1
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-08-14 20:24:15 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router'
+import './App.css'
+import CommonLayout from './components/layout/CommonLayout'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <CommonLayout>
+          <Outlet />
+        </CommonLayout>
+      </div>
+    </>
+  )
+}
+
+export default App
