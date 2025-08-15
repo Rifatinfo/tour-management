d28447a4a754ff38ede86487ab7afad2c197b3fc
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-08-16 00:21:52 +0600

    add redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import { router } from './routes'
-import { ThemeProvider } from './providers/theme.provider'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./routes";
+import { ThemeProvider } from "./providers/theme.provider";
+import { Provider } from 'react-redux'
+import { store } from "./redux/store";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
+  </StrictMode>
+);
